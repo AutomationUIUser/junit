d86eea8ed6533491fe7cfcd2fd402e7325ab1430
dipak <dipak.prajapati@opshub.com>
2020-03-04 12:57:03 +0530

    init commit

---
+++ src/test/java/junit/test/MathUtilsTest.java
@@ -0,0 +1,20 @@
+package junit.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test1() {
+		System.out.println("Test case failed");
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void test2() {
+		System.out.println("Test case passed");
+	}
+
+}
